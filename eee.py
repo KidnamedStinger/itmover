e62d52498793ffd240c148d9800d94e5f96eb0cb
skizzy <130990031+KidnamedStinger@users.noreply.github.com>
2025-09-19 17:15:16 +0300

    pervaya laba ura :happy:

--- eee.py
+++ eee.py
@@ -1,19 +1,29 @@
-nums = [2, 4, 6, 10, 3, 5, 1, 7]
-target = 8
-t = True
-for i in range(len(nums)):
-    n1 = nums[i]
-    for j in range(len(nums)):
-        if i == j:
-            pass
-        else:
-            n2 = nums[j]
-            if n1 + n2 == target:
-                print(i, j)
-                t = False
-if t:
-    print("пар нет")
-    
+import unittest
+
+def summa(nums, target):
+    for i in range(len(nums)):
+        a = nums[i]
+        for j in range(len(nums)):
+            if j == i:
+                pass
+            else:
+                b = nums[j]
+                if a + b == target:
+                    return [i, j]
+    return "Пары нет"
+
+class TestMath(unittest.TestCase):
+    def test1(self):
+        self.assertEqual(summa([3, 3], 6), [0, 1])
+
+    def test2(self):
+        self.assertEqual(summa([2,7,11,15], 9), [0, 1])
+
+    def test3(self):
+        self.assertEqual(summa([3,2,4], 6), [1, 2])
+
+unittest.main(argv=[''], verbosity=2, exit=False)
 
     
+
 
