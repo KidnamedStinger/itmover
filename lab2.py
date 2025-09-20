f313b6d6137e9ac41b5accbb860041918cbc06a9
skizzy <130990031+KidnamedStinger@users.noreply.github.com>
2025-09-20 17:17:40 +0300

    lab2.py

--- lab2.py
+++ lab2.py
@@ -1,16 +1,37 @@
 a = []
 count = 0
+pref = 0
 
-def binguessnumber(target, start, finish, count  = count):
-    # создание списка случайных чисел по указанному интервалу
+def linguessnumber(target, start, finish, count = count):
+    """ функция ищет загаданное число линейным поиском и возвращает количество шагов и само число """
+
     for i in range(start, finish+1):
         a.append(i)
-    # индексы первого, последнего и среднего чисел
+
+    for i in range(len(a)):
+        if a[i] == target:
+            count += 1
+            break
+        else:
+            count += 1
+    return(target, count)
+
+
+def binguessnumber(target, start, finish, count = count):
+    
+    """ функция ищет загаданное число бинарным поиском и вовзращает количество шагов и само число """
+    
+    for i in range(start, finish+1):
+        a.append(i)
+        
+    """ индексы первого, среднего и последнего эелементов списка """
+    
     low = 0
     mid = len(a) // 2
     high = len(a) - 1
     
     while a[mid] != target and low <= high:
+        
         if target > a[mid]:
             low = mid + 1
             count += 1
@@ -18,16 +39,24 @@ def binguessnumber(target, start, finish, count  = count):
             high = mid - 1
             count += 1
         mid = (low + high) // 2
+        
     if low > high:
         return(none, none)
     else:
         return(target, count)
 
 
-def main(target = int(input()), start = int(input()), finish = int(input())):
+def main(target = int(input()), start = int(input()), finish = int(input()), pref = int(input())):
 
-    target, count = binguessnumber(target, start, finish)
+    """ переменная pref отвечает за выбор предпочтительного способа поиска загаданного числа, 1 - бинарный, 2 - линейный """
+    if pref == 1:
+        target, count = binguessnumber(target, start, finish)
+        
+    if pref == 2:
+        target, count = linguessnumber(target, start, finish)
     
     return(target, count)
 
 print(main())
+
+
