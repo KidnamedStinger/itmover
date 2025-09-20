37f58a31a9a3ba2fc967e57e2cf845b5f5503095
skizzy <130990031+KidnamedStinger@users.noreply.github.com>
2025-09-20 16:14:50 +0300

    vtoraya laba :frown:

---
+++ lab2.py
@@ -0,0 +1,33 @@
+a = []
+count = 0
+
+def guessnumber(target, start, finish, count  = count):
+    # создание списка случайных чисел по указанному интервалу
+    for i in range(start, finish+1):
+        a.append(i)
+    #индексы первого, последнего и среднего чисел
+    low = 0
+    mid = len(a) // 2
+    high = len(a) - 1
+    
+    while a[mid] != target and low <= high:
+        if target > a[mid]:
+            low = mid + 1
+            count += 1
+        else:
+            high = mid - 1
+            count += 1
+        mid = (low + high) // 2
+    if low < high:
+        return "no value"
+    else:
+        return(target, count)
+
+
+def main(target = int(input()), start = int(input()), finish = int(input())):
+
+    target, count = guessnumber(target, start, finish)
+    
+    return(target, count)
+
+print(main())
